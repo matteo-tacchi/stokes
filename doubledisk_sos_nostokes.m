af615807cb77569bee311faa05a62616c4e132af
matteo-tacchi <52538699+matteo-tacchi@users.noreply.github.com>
2023-05-09 17:23:33 +0200

    Create doubledisk_sos_nostokes.m

---
+++ doubledisk_sos_nostokes.m
@@ -0,0 +1,38 @@
+% Approximation of the area of a double planar disk
+% with SOS and without Stokes constraints
+% D. Henrion, M. Tacchi, 1 Feb 22
+
+% uses YALMIP for modeling the SOS problem
+% and MOSEK for solving the SDP problem
+
+d = 16;  % relaxation degree
+
+x = sdpvar(2,1);
+b = 1-x'*x;
+g = (1/16-(x(1)-1/2)^2-x(2)^2)*((x(1)+1/2)^2+x(2)^2-1/16);
+[w,wc] = polynomial(x,d);
+[sb,sbc] = polynomial(x,d-2);
+[sk,skc] = polynomial(x,d-4);
+pows = genpow(3,d); pows = pows(:,2:end);
+y = momball(pows);
+ops = sdpsettings('solver','mosek');
+solvesos([sos(w-sb*b),sos(sb),sos(w-1-sk*g),sos(sk)],y'*wc,ops,[wc;sbc;skc]);
+
+double(y'*wc)
+
+P = vectorize(sdisplay(w));
+P = strrep(P,'x(1)','X1');
+P = strrep(P,'x(2)','X2');
+[X1,X2] = meshgrid(linspace(-1,1,1e2));
+P = double(eval(P));
+P(X1.^2+X2.^2>=0.95)=0;
+close all
+surf(X1,X2,P,'FaceColor','interp','EdgeColor','none','FaceLighting','phong');
+axis([-1 1 -1 1 0 1.5]);
+view(-60,50)
+axis vis3d
+camlight left
+colormap spring
+xlabel x_1
+ylabel x_2
+zlabel w
